b26baca748ba45aa3df77c19deac3dcc27fe96f3
aria <aria@elk.wtf>
2016-11-11 13:18:52 -0500

    Fixed object-shorthand syntax for utils export

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import Row from './components/Row';
 import { divvy, media } from './utils';
 
 const utils = {
-  [divvy]: divvy,
-  [media]: media
+  divvy,
+  media
 };
 
 export { Column, Row, utils };
