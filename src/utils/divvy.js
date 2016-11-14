2c5b28390e8ef01c0e5e24cadeb8085dcee0e652
Nick Franciosi <nickfranciosi@gmail.coml>
2016-11-13 19:46:05 -0600

    makes 12 the default parameter in the divvy function

--- src/utils/divvy.js
+++ src/utils/divvy.js
@@ -3,11 +3,6 @@
 // This can then be used by the grid system.
 // i.e. width: ${divvy(12) * 4};
 // This will generate a column 4 units wide.
-export default function(divisions) {
-  // TODO: add arg for computing offset.
-  if (divisions === undefined) {
-    return 100 / 12;
-  }
+export default function(divisions = 12) {
   return 100 / divisions;
 }
-
