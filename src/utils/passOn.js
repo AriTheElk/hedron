a2bce861d5308bba5085c16c06c384c8ac4f0f99
aria <aria@elk.wtf>
2016-11-14 03:12:58 -0500

    Fixed eslint errors
    * `arrow-parens` in callback argument

--- src/utils/passOn.js
+++ src/utils/passOn.js
@@ -3,7 +3,7 @@ import React from 'react';
 // The passOn function applies a specific set of properties
 // to the children components. It will only apply the props
 // to a component type that exists in the ofTypes array.
-export default function(children, ofTypes, props, callback = (r) => r) {
+export default function(children, ofTypes, props, callback = r => r) {
   const response = React.Children.map(children,
     child => React.cloneElement(child,
       ofTypes.includes(child.type) ? props : { }
