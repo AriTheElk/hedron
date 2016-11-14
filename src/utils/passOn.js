aaffb06fbec3b3f8f05853c7ce4c3b5910231870
aria <aria@elk.wtf>
2016-11-14 02:17:52 -0500

    Added new passOn utility
    The passOn() utility is great for reverse-inheritance of properties.
    It accepts a list of component types, and will only pass the given
    properties onto children components that match the type!

---
+++ src/utils/passOn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// The passOn function applies a specific set of properties
+// to the children components. It will only apply the props
+// to a component type that exists in the ofTypes array.
+export default function(children, ofTypes, props, callback = (r) => r) {
+  const response = React.Children.map(children,
+    child => React.cloneElement(child,
+      ofTypes.includes(child.type) ? props : { }
+    )
+  );
+  return callback(response);
+}
+
