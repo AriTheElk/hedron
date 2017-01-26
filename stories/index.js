2a87a48b56d42e1fde13fe59c24970496dc538d2
Garet McKinley <self@mediachicken.com>
2017-01-26 08:25:54 -0500

    add storybook tests for BreakpointProvider
    - add test for `Default breakpoints`
    - add test for Bootstrap 3 breakpoints

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import { Page, Row, Column, Hidden } from '../src';
+import { Page, Row, Column, Hidden, BreakpointProvider } from '../src';
 
 storiesOf('Layouts', module)
   .add('Column Sizes (xs)', () => (
@@ -192,4 +192,58 @@ storiesOf('Hidden', module)
       </p>
       <Hidden lg><Row><Column /></Row></Hidden>
     </Page>
+  ));
+
+storiesOf('BreakpointProvider', module)
+  .add('Default breakpoints', () => (
+    <BreakpointProvider>
+      <Page debug>
+        <p>
+          This layout utilizes similar breakpoints as bootstrap 3:
+          576, 768, and 1200.
+        </p>
+        <Row>
+          <Column>Header</Column>
+        </Row>
+        <Row>
+          <Column sm={3}>Sidebar</Column>
+          <Column sm={9} fluid>
+            <Row>
+              <Column md={6} lg={4}>A blog post</Column>
+              <Column md={6} lg={4}>Another blog post</Column>
+              <Column md={6} lg={4}>Yet another blog post</Column>
+            </Row>
+          </Column>
+        </Row>
+        <Row>
+          <Column>Footer</Column>
+        </Row>
+      </Page>
+    </BreakpointProvider>
+  ))
+  .add('Bootstrap 3', () => (
+    <BreakpointProvider breakpoints={{ sm: 576, md: 768, lg: 1200 }}>
+      <Page debug>
+        <p>
+          This layout utilizes similar breakpoints as bootstrap 3:
+          576, 768, and 1200.
+        </p>
+        <Row>
+          <Column>Header</Column>
+        </Row>
+        <Row>
+          <Column sm={3}>Sidebar</Column>
+          <Column sm={9} fluid>
+            <Row>
+              <Column md={6} lg={4}>A blog post</Column>
+              <Column md={6} lg={4}>Another blog post</Column>
+              <Column md={6} lg={4}>Yet another blog post</Column>
+            </Row>
+          </Column>
+        </Row>
+        <Row>
+          <Column>Footer</Column>
+        </Row>
+      </Page>
+    </BreakpointProvider>
   ));
