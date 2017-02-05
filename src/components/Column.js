6599f6ee4f4a3e02572d2fdc04ff81f0f82c1a12
Garet McKinley <self@mediachicken.com>
2017-02-05 09:21:44 -0500

    use outline instead of border in debug mode (#62)

--- src/components/Column.js
+++ src/components/Column.js
@@ -54,7 +54,7 @@ const Column = styled(ColumnContainer)`
   display: block;
   ${props => props.debug
     ? `background-color: rgba(50, 50, 255, .1);
-       border: 1px solid #fff;`
+       outline: 1px solid #fff;`
     : ''
   }
   box-sizing: border-box;
