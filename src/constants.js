a265444dc93dd3e39420b8e08531cb00dadfce8a
aria <aria@elk.wtf>
2017-04-01 21:32:24 -0400

    add vAlign prop

--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,23 @@
 // this is the object that holds the css translation
 // rules for hedron's 1.0.0 API
 export const flexCSS = {
-  align: {
-    rules: {
+  hAlign: {
+    directions: {
       horizontal: "justify-content",
       vertical: "align-items"
+    },
+    values: {
+      start: "flex-start",
+      end: "flex-end",
+      center: "center",
+      stretch: "stretch",
+      spaced: "space-between"
+    }
+  },
+  vAlign: {
+    directions: {
+      horizontal: "align-items",
+      vertical: "justify-content"
     },
     values: {
       start: "flex-start",
