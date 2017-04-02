bfbf23bb8f3a34adbbd838f795393b876a9d8142
Garet McKinley <self@mediachicken.com>
2017-04-02 18:47:37 -0400

    add debug style generator

--- src/components/Box.js
+++ src/components/Box.js
@@ -11,15 +11,23 @@ const compute = name =>
       ${shift ? `margin-left: ${shift};` : ""}
     `)(props.divisions, props[name], props[`${name}Shift`]));
 
+const debug = debug => {
+  if (!debug) return;
+  const styles = [];
+  if (debug.enabled) {
+    styles.push('background: rgba(0, 0, 100, .15);');
+    styles.push('outline: 1px solid #fff;');
+  }
+  return styles;
+};
+
 const Box = styled.div`
+  box-sizing: border-box;
   display: ${props => props.noFlex ? "block" : "flex"};
   flex-direction: column;
   flex: ${props => props.flex ? props.flex : "1 1 auto"};
   overflow: scroll;
-  ${props => props.debug && (props.debug.enabled === true && "background: rgba(0, 0, 100, .15);")}
-  ${props => props.debug &&
-    (props.debug.enabled && (props.debug.border ? `outline: ${props.debug.border};` : "outline: 1px solid #fff;"))})
-  box-sizing: border-box;
+  ${props => debug(props.debug)}
   ${props => props.grow && `flex-grow: ${props.grow === true ? "1" : props.grow};`}
   ${props => props.gutter && !props.fluid ? `padding: ${props.gutter}px ${props.gutter / 2}px;` : ""}
   ${compute("xs")}
