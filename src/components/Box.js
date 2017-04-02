2c89c321b6082bc29494a53d875f20955a501120
Garet McKinley <self@mediachicken.com>
2017-04-02 19:21:11 -0400

    add applyStyle utility

--- src/components/Box.js
+++ src/components/Box.js
@@ -2,24 +2,15 @@
 import styled from "styled-components";
 import { withLayout } from "./LayoutProvider";
 import { breakpoint } from "../utils";
+import { applyStyle } from '../utils/styles';
 
 const compute = name =>
   breakpoint(name, (props, name) =>
     ((divisions, size: string, shift: string, gutter) =>
       `
-      ${size ? size === "grow" ? "flex-grow: 1;" : `flex-grow: 0; width: ${size};` : ""}
-      ${shift ? `margin-left: ${shift};` : ""}
+      ${applyStyle(size, [`width: ${size}`])};
+      ${applyStyle(shift, [`margin-left: ${shift}`])};
     `)(props.divisions, props[name], props[`${name}Shift`]));
-
-const debug = debug => {
-  if (!debug) return;
-  const styles = [];
-  if (debug.enabled) {
-    styles.push('background: rgba(0, 0, 100, .15);');
-    styles.push('outline: 1px solid #fff;');
-  }
-  return styles;
-};
 
 const Box = styled.div`
   box-sizing: border-box;
@@ -27,9 +18,9 @@ const Box = styled.div`
   flex-direction: column;
   flex: ${props => props.flex ? props.flex : "1 1 auto"};
   overflow: scroll;
-  ${props => debug(props.debug)}
-  ${props => props.grow && `flex-grow: ${props.grow === true ? "1" : props.grow};`}
-  ${props => props.gutter && !props.fluid ? `padding: ${props.gutter}px ${props.gutter / 2}px;` : ""}
+  ${props => applyStyle(props.debug, ['background: rgba(0, 0, 100, .15)', 'outline: 1px solid #fff'])};
+  ${props => applyStyle(props.grow, [`flex-grow: ${props.grow === true ? "1" : props.grow};`])};
+  ${props => applyStyle(props.gutter, [`padding: ${props.gutter}px ${props.gutter / 2}px;`])};
   ${compute("xs")}
   ${compute("sm")}
   ${compute("md")}
--- src/utils/index.js
+++ src/utils/index.js
@@ -2,5 +2,6 @@
 import media, { defaultBreakpoints } from "./media";
 import breakpoint from "./breakpoint";
 import * as flex from "./flex";
+import * as styles from './styles';
 
-export { media, defaultBreakpoints, breakpoint, flex };
+export { media, defaultBreakpoints, breakpoint, flex, styles };
