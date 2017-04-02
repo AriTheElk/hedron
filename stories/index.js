5564c767f1209f566134a7b4af7dffed4c2f39d8
Garet McKinley <self@mediachicken.com>
2017-04-01 22:53:18 -0400

    add support for px/percentage based shift props

--- stories/index.js
+++ stories/index.js
@@ -126,23 +126,10 @@ storiesOf('Vertical Boxes', module)
   .add('Box Shift (xs)', () => (
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
-        <p>
-          These boxes have no set size, only an xsShift property.
-        They automatically flex based on the amount of room left.
-      </p>
-        <Container>
-          <Box xsShift={0} />
-          <Box xsShift={1} />
-          <Box xsShift={2} />
-          <Box xsShift={3} />
-          <Box xsShift={4} />
-          <Box xsShift={5} />
-          <Box xsShift={6} />
-          <Box xsShift={7} />
-          <Box xsShift={8} />
-          <Box xsShift={9} />
-          <Box xsShift={10} />
-          <Box xsShift={11} />
+        <Container direction="vertical">
+          <Box xsShift="100px">xsShift="100px"</Box>
+          <Box xsShift="50%">xsShift="50%"</Box>
+          <Box xsShift="auto">xsShift="auto"</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -150,23 +137,10 @@ storiesOf('Vertical Boxes', module)
   .add('Box Shift (sm)', () => (
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
-        <p>
-          These boxes have no set size, only an smShift property.
-        They automatically flex based on the amount of room left.
-      </p>
-        <Container>
-          <Box smShift={0} />
-          <Box smShift={1} />
-          <Box smShift={2} />
-          <Box smShift={3} />
-          <Box smShift={4} />
-          <Box smShift={5} />
-          <Box smShift={6} />
-          <Box smShift={7} />
-          <Box smShift={8} />
-          <Box smShift={9} />
-          <Box smShift={10} />
-          <Box smShift={11} />
+        <Container direction="vertical">
+          <Box smShift="100px">smShift="100px"</Box>
+          <Box smShift="50%">smShift="50%"</Box>
+          <Box smShift="auto">smShift="auto"</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -174,23 +148,10 @@ storiesOf('Vertical Boxes', module)
   .add('Box Shift (md)', () => (
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
-        <p>
-          These boxes have no set size, only an mdShift property.
-        They automatically flex based on the amount of room left.
-      </p>
-        <Container>
-          <Box mdShift={0} />
-          <Box mdShift={1} />
-          <Box mdShift={2} />
-          <Box mdShift={3} />
-          <Box mdShift={4} />
-          <Box mdShift={5} />
-          <Box mdShift={6} />
-          <Box mdShift={7} />
-          <Box mdShift={8} />
-          <Box mdShift={9} />
-          <Box mdShift={10} />
-          <Box mdShift={11} />
+        <Container direction="vertical">
+          <Box mdShift="100px">mdShift="100px"</Box>
+          <Box mdShift="50%">mdShift="50%"</Box>
+          <Box mdShift="auto">mdShift="auto"</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -198,23 +159,10 @@ storiesOf('Vertical Boxes', module)
   .add('Box Shift (lg)', () => (
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
-        <p>
-          These boxes have no set size, only an lgShift property.
-        They automatically flex based on the amount of room left.
-      </p>
-        <Container>
-          <Box lgShift={0} />
-          <Box lgShift={1} />
-          <Box lgShift={2} />
-          <Box lgShift={3} />
-          <Box lgShift={4} />
-          <Box lgShift={5} />
-          <Box lgShift={6} />
-          <Box lgShift={7} />
-          <Box lgShift={8} />
-          <Box lgShift={9} />
-          <Box lgShift={10} />
-          <Box lgShift={11} />
+        <Container direction="vertical">
+          <Box lgShift="100px">lgShift="100px"</Box>
+          <Box lgShift="50%">lgShift="50%"</Box>
+          <Box lgShift="auto">lgShift="auto"</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -226,21 +174,21 @@ storiesOf('Horizontal Boxes', module)
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
         <p>
-          These boxes have only have the xs property set.
+          These boxes have xs="grow" set on them
       </p>
         <Container direction="horizontal">
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
-          <Box xs={1} />
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
+          <Box xs="grow">Test</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -249,21 +197,21 @@ storiesOf('Horizontal Boxes', module)
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
         <p>
-          These boxes have only have the sm property set.
+          These boxes have sm="grow" set on them
       </p>
         <Container direction="horizontal">
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
-          <Box sm={1} />
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
+          <Box sm="grow">Test</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -272,21 +220,21 @@ storiesOf('Horizontal Boxes', module)
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
         <p>
-          These boxes have only have the md property set.
-        </p>
+          These boxes have md="grow" set on them
+      </p>
         <Container direction="horizontal">
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
-          <Box md={1} />
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
+          <Box md="grow">Test</Box>
         </Container>
       </Section>
     </LayoutProvider>
@@ -295,21 +243,21 @@ storiesOf('Horizontal Boxes', module)
     <LayoutProvider debug={{ enabled: true }} gutter={10}>
       <Section>
         <p>
-          These boxes have only have the lg property set.
+          These boxes have lg="grow" set on them
       </p>
         <Container direction="horizontal">
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
-          <Box lg={1} />
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
+          <Box lg="grow">Test</Box>
         </Container>
       </Section>
     </LayoutProvider>
