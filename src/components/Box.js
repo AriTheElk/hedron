edbd15b1a615e64293ec789e88459352d7910e95
aria <aria@elk.wtf>
2017-04-02 19:33:08 -0400

    update addStyle utility

--- src/components/Box.js
+++ src/components/Box.js
@@ -16,11 +16,11 @@ const Box = styled.div`
   box-sizing: border-box;
   display: ${props => props.noFlex ? "block" : "flex"};
   flex-direction: column;
-  flex: ${props => props.flex ? props.flex : "1 1 auto"};
   overflow: scroll;
-  ${props => applyStyle(props.debug, ['background: rgba(0, 0, 100, .15)', 'outline: 1px solid #fff'])};
-  ${props => applyStyle(props.grow, [`flex-grow: ${props.grow === true ? "1" : props.grow};`])};
-  ${props => applyStyle(props.gutter, [`padding: ${props.gutter}px ${props.gutter / 2}px;`])};
+  ${props => applyStyle(props.flex, [`flex: ${props.flex}`])};
+  ${props => applyStyle(props.debug && props.debug.enabled, ['background: rgba(0, 0, 100, .15)', 'outline: 1px solid #fff'])};
+  ${props => applyStyle(props.grow, [`flex-grow: ${props.grow === true ? "1" : props.grow}`])};
+  ${props => applyStyle(props.gutter && !props.fluid, [`padding: ${props.gutter}px ${props.gutter / 2}px`])};
   ${compute("xs")}
   ${compute("sm")}
   ${compute("md")}
