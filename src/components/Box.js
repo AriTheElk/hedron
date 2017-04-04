a37140ece06f52cbaa1f1592c8c64b586453ada1
Garet McKinley <self@mediachicken.com>
2017-04-03 21:17:37 -0400

    add applyBreakpointStyle utility

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,25 +1,36 @@
 // @flow
 import styled from "styled-components";
 import { withLayout } from "./LayoutProvider";
-import { breakpoint } from "../utils";
+import { breakpoint, applyBreakpointStyle } from "../utils";
 import { applyStyle } from '../utils/styles';
+
+const applyWidth = (rule, value, defaultValue = '') => {
+  switch (typeof value) {
+  case 'string':
+    return `${rule}: ${value};`;
+  case 'object':
+    return Object.keys(value).map(key =>
+      breakpoint(key, (props, name) => `${rule}: ${value[key]};`)
+    );
+  default:
+    return defaultValue;
+  }
+}
 
 const compute = name =>
   breakpoint(name, (props, name) =>
-    ((divisions, size: string, shift: string, gutter) =>
-      `
-      ${applyStyle(size, [`width: ${size}`])};
-      ${applyStyle(shift, [`margin-left: ${shift}`])};
-    `)(props.divisions, props[name], props[`${name}Shift`]));
+    `${applyBreakpointStyle('width', props.width, name)};
+     ${applyBreakpointStyle('margin-left', props.shift, name)};
+     ${applyBreakpointStyle('flex-grow', props.grow, name)};
+     ${applyBreakpointStyle('flex', props.flex, name)};`
+  );
 
 const Box = styled.div`
   box-sizing: border-box;
   display: ${props => props.noFlex ? "block" : "flex"};
   flex-direction: column;
   overflow: auto;
-  ${props => applyStyle(props.flex, [`flex: ${props.flex}`])};
   ${props => applyStyle(props.debug && props.debug.enabled, ['background: rgba(0, 0, 100, .15)', 'outline: 1px solid #fff'])};
-  ${props => applyStyle(props.grow, [`flex-grow: ${props.grow === true ? "1" : props.grow}`])};
   ${props => applyStyle(props.gutter && !props.fluid, [`padding: ${props.gutter}px ${props.gutter / 2}px`])};
   ${compute("xs")}
   ${compute("sm")}
