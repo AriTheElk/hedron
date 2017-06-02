c08728a4933e126957305fc9518b334d96d27b5d
Garet McKinley <hello@mediachicken.com>
2017-06-02 15:49:05 -0400

    switch Flex back to Container

--- stories/Examples/VSCode/index.js
+++ stories/Examples/VSCode/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Highlight from 'react-highlight';
-import { LayoutProvider, Section, Flex, Box, Hidden } from '../../../src';
+import { LayoutProvider, Section, Container, Box, Hidden } from '../../../src';
 import Compass from './Compass';
 
 const Example = `<LayoutProvider debug={{ enabled: true }} gutter="10px">
     <Section fluid>
-      <Flex direction="vertical" height="100vh">
+      <Container direction="vertical" height="100vh">
         <Box>Header</Box>
         <Box grow={1} fluid>
-          <Flex grow={1} wrap={{ xs: true, sm: false }}>
+          <Container grow={1} wrap={{ xs: true, sm: false }}>
             <Box
               width={{ xs: '100%', sm: "45px" }}
               height={{ xs: "45px", sm: "auto" }}
@@ -19,10 +19,10 @@ const Example = `<LayoutProvider debug={{ enabled: true }} gutter="10px">
               <h2>Here's the source code for this layout</h2>
               <Highlight className="xml">{Example}</Highlight>
             </Box>
-          </Flex>
+          </Container>
         </Box>
         <Box>Footer</Box>
-      </Flex>
+      </Container>
     </Section>
   </LayoutProvider>`;
 
@@ -30,10 +30,10 @@ const VSCode = (props) =>
   <LayoutProvider debug={{ enabled: true }} gutter="10px">
     <Section fluid>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.10.0/styles/atelier-estuary-dark.min.css" />
-      <Flex direction="vertical" height="100vh">
+      <Container direction="vertical" height="100vh">
         <Box>Header</Box>
         <Box grow shrink fluid>
-          <Flex grow wrap={{ xs: true, sm: false }}>
+          <Container grow wrap={{ xs: true, sm: false }}>
             <Box
               width={{ xs: '100%', sm: "45px" }}
               height={{ xs: "45px", sm: "auto" }}
@@ -44,10 +44,10 @@ const VSCode = (props) =>
               <h2>Here's the source code for this layout</h2>
               <Highlight className="xml">{Example}</Highlight>
             </Box>
-          </Flex>
+          </Container>
         </Box>
         <Box>Footer</Box>
-      </Flex>
+      </Container>
     </Section>
   </LayoutProvider>
 
