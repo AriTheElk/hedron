c4e5d4c3db717a7d93b1d8d6d6e032c2e49725ff
Wojtek Cichoradzki <wojciech.cichoradzki@gmail.com>
2018-08-11 17:27:34 +0100

    fix: use updated storybook dependency, ignore dev dependency in stories

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { storiesOf } from '@kadira/storybook';
+import { storiesOf } from '@storybook/react';
 import { Page, Row, Column, Hidden, BreakpointProvider } from '../src';
 
 storiesOf('Layouts', module)
