5bf4a7b3c862c4cd072b83e01e775e997f3cdbe9
aria <aria@elk.wtf>
2018-09-06 12:55:19 -0500

    port @tetrahedron/grid to hedron 1.0

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,70 +1,35 @@
-// @flow
-import styled from "styled-components";
-import { withLayout } from "./LayoutProvider";
-import { breakpoint, applyBreakpointStyle } from "../utils";
-import { applyStyle } from '../utils/styles';
-import hexToRgb from '../utils/hexToRgb';
+import PropTypes from "prop-types";
+import styled, { css } from "styled-components";
 
-const applyWidth = (rule, value, defaultValue = '') => {
-  switch (typeof value) {
-  case 'string':
-    return `${rule}: ${value};`;
-  case 'object':
-    return Object.keys(value).map(key =>
-      breakpoint(key, (props, name) => `${rule}: ${value[key]};`)
-    );
-  default:
-    return defaultValue;
-  }
-}
+import { flex, drawDebug, breakpoints } from "../helpers";
+import { withBreakpoints } from "./Provider";
+import Base from "./Base";
 
-const debugStyles = {
-  outline: '1px solid #fff',
-  background: 'rgba(0, 0, 100, .15)'
-};
-
-const toRgba = (rbg) => {
-  const result = [];
-
-  rbg.map((n) => result.push(String(n)));
-  result.push('.15');
-
-  return result.join(',');
-}
-const getDebugStyles = (props) => {
-  const outline = props.debug.outline ? props.debug.outline : debugStyles.outline;
-  const background = /^#/.test(props.debug.fill) ? `rgba(${toRgba(hexToRgb(props.debug.fill))})` : debugStyles.background;
-
-  return [`background: ${background}`, `outline: ${outline}`];
-};
-
-const compute = name =>
-  breakpoint(name, (props, name) => [
-    applyBreakpointStyle('width', props.width, name),
-    applyBreakpointStyle('height', props.height, name),
-    applyBreakpointStyle('margin-left', props.shift, name),
-    applyBreakpointStyle('flex-grow', props.grow, name, { on: 1, off: 0 }),
-    applyBreakpointStyle('flex-shrink', props.shrink, name, { on: 1, off: 0 }),
-    applyBreakpointStyle('flex', props.flex, name),
-    applyBreakpointStyle('padding', props.gutter, name),
-    applyBreakpointStyle('padding', props.fluid, name, { on: '0', off: props.gutter }),
-  ]);
-
-const Box = styled.div`
-  box-sizing: border-box;
-  display: ${props => props.block ? "block" : "flex"};
-  flex-direction: column;
-  overflow: ${props => props.overflow ? props.overflow : "auto"};
-  ${props => applyStyle(props.debug && props.debug.enabled, getDebugStyles(props))};
-
-  ${compute("xs")}
-  ${compute("sm")}
-  ${compute("md")}
-  ${compute("lg")}
+const Box = styled(Base("div"))`
+  ${props => props.fluid && "padding: 0;"} box-sizing: border-box;
+  ${props => props.debug && drawDebug()};
+  ${props => flex(props)};
+  ${props => breakpoints(props)};
 `;
 
-Box.defaultProps = {
-  shrink: true
+Box.propTypes = {
+  // Debug Properties
+  debug: PropTypes.bool,
+  // Flex-specific Properties
+  flex: PropTypes.bool,
+  fill: PropTypes.bool,
+  fluid: PropTypes.bool,
+  shiftRight: PropTypes.bool,
+  shiftLeft: PropTypes.bool,
+  shiftUp: PropTypes.bool,
+  shiftDown: PropTypes.bool,
+  ...Base.propTypes,
 };
 
-export default withLayout(Box);
+Box.defaultProps = {
+  debug: false,
+  fill: false,
+  ...Base.defaultProps,
+};
+
+export default withBreakpoints(Box);
